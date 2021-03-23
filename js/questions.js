e9226d658eafe5234306e0cb6b94c828af0b848b
Emilio Luna <emilioluna1997@gmail.com>
2021-03-22 19:43:08 -0700

    update repo

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Which company developed JavaScript?",
+    choice1: "Bell Labs",
+    choice2: "Netscape",
+    choice3: "IBM",
+    choice4: "Google",
+    answer: 1
+},
+            {
+            question: "Which Is Not A JavaScript Data Type?",
+    choice1: "Number",
+    choice2: "Function",
+    choice3: "String",
+    choice4: "Object",
+    answer: 2
+},
+            {
+            question: "Which variable is visible everywhere in your JavaScript code?",
+    choice1: "Global",
+    choice2: "Local",
+    choice3: "Both",
+    choice4: "Neither",
+    answer: 1
+},
+            {
+            question: "Which built-in method sorts the elements of an array?",
+    choice1: "sort()",
+    choice2: "order()",
+    choice3: "changeOrder()",
+    choice4: "None of the Above",
+    answer: 0
+},
+            {
+            question: "How do you insert a comment in a CSS file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 1
+},
+            {
+            question: "Which is not a Javascript framework or library?",
+    choice1: "jQuery",
+    choice2: "Polymer",
+    choice3: "Cassandra",
+    choice4: " Meteor",
+    answer: 2
+},
+            {
+               question: "Why do JavaScript and Java have similar name?",
+    choice1: "JavaScript is a stripped-down version of Java",
+    choice2: "JavaScript's syntax is loosely based on Java's",
+    choice3: "They both originated on the island of JavaBackground",
+    choice4: "None of the above",
+    answer: 1
+},{
+               question: "How to write an IF statement in JavaScript?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+            {
+               question: "Inside which HTML element do we put the JavaScript?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+            {
+               question: "How does a WHILE loop start?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
